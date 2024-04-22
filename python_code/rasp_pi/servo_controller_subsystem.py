3c5679bd66117ea3c500872234c8e1bfdc30ca5d
Tyler Hirsch <70651139+THirsch62@users.noreply.github.com>
2024-04-22 13:54:24 -0500

    Update servo_controller_subsystem.py

--- python_code/rasp_pi/servo_controller_subsystem.py
+++ python_code/rasp_pi/servo_controller_subsystem.py
@@ -1,55 +1,14 @@
-from gpiozero import Servo
-from threading import Thread
 import time
+from utils import *
 
-FOLD_ANGLE = 120
-
-class Servo_Custom(Servo):
-    # angle should be between -90 and 90
-    def set_angle(self, angle):
-        self.value = angle / 90
-    
-    def offset_angle(self, angle):
-        self.value += angle / 90
-
-# TODO
-# Add servo pins
-# dictionary with panels as keys and corresponding servos as values
-panel_servos = {
-    1: (Servo_Custom(), Servo_Custom()),
-    2: (Servo_Custom(), Servo_Custom()),
-    3: (Servo_Custom(), Servo_Custom()),
-    4: (Servo_Custom(), Servo_Custom()),
-    5: (Servo_Custom(), Servo_Custom()),
-    6: (Servo_Custom(), Servo_Custom()),
-}
-
-# TODO
 # dictionary with folding patterns
 folding_pattern = {
-    "T-shirt/Top": [[]],
-    "Trouser": [[]],
-    "Pullover": [[]]
+    "T-shirt/Top": [[1, 4], [3, 6], [2], [5]],
+    "Trouser": [[1, 4], [3, 6], [2], [5]],
+    "Pullover": [[1, 4], [3, 6], [2], [5]]
 }
-
-def fold_panel(panel):
-    left, right = panel_servos[panel]
-    # fold panel up and wait for servo to rotate
-    left.offset_angle(-120)
-    right.offset_angle(120)
-    time.sleep(0.5)
-    # restore panel to original position and wait for servo to rotate
-    left.offset_angle(120)
-    right.offset_angle(-120)
-    time.sleep(0.5)
 
 # TODO
 def main(clothing_item):
     for panels in folding_pattern[clothing_item]:
-        threads = []
-        for panel in panels:
-            thread = Thread(target=fold_panel, args=[panel])
-            thread.start()
-            threads.append(thread)
-        for thread in threads:
-            thread.join()
+        fold_panel(panels)
